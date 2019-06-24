83c0f37c371d7ad8f209b48d9975acb562068427
rsun-eyelit <rsun@eyelit.com>
2019-06-24 11:05:16 -0400

    add resource and logback, log4j

--- slf4j/src/main/java/roger/slf4j/LoggerRoot.java
+++ slf4j/src/main/java/roger/slf4j/LoggerRoot.java
@@ -5,11 +5,13 @@ import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class LoggerRoot {
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
 	
 	public static void main(String... args) {
+		MDC.put("app",  "RogerLogger");
 		IntStream.rangeClosed(1,  10).forEach(counter -> {
 			logger.info("Counter: " + counter);
 		});
---
+++ slf4j/src/test/java/roger/slf4j/LoggerRootTest.java
@@ -0,0 +1,12 @@
+package roger.slf4j;
+
+import org.junit.Test;
+
+public class LoggerRootTest {
+
+	@Test
+	public void test1() {
+		LoggerRoot.main();
+	}
+	
+}
