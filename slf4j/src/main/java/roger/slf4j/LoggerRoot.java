38de2db2b8d7c64cbb205c58077d379bdee20e9f
rsun-eyelit <rsun@eyelit.com>
2019-07-19 14:57:13 -0400

    backup

--- slf4j/src/main/java/roger/slf4j/LoggerRoot.java
+++ slf4j/src/main/java/roger/slf4j/LoggerRoot.java
@@ -1,6 +1,7 @@
 package roger.slf4j;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
@@ -20,5 +21,25 @@ public class LoggerRoot {
 		IntStream.rangeClosed(1,  10).forEach(counter -> {
 			logger.info("Counter: " + counter);
 		});
+		
+		new LoggerRoot().testNullable();
+	}
+	
+	public LoggerRoot() {} {
+		String value = testNullable();
+		logger.info(value);
+	}
+	
+	private String testNullable() {
+		String strA = null;
+		String strB = "test";
+		Optional<String> oStrA = Optional.ofNullable(strA);
+		Optional<String> oStrB = Optional.ofNullable(strB);
+		
+		return "A+B: " + oStrA.orElse("Empty") + " " + oStrB.orElse("Empty");
+	}
+	
+	public float divide(float f, float f2) {
+		return f/f2;
 	}
 }
--- slf4j/src/test/java/roger/slf4j/LoggerRootTest.java
+++ slf4j/src/test/java/roger/slf4j/LoggerRootTest.java
@@ -1,12 +1,31 @@
 package roger.slf4j;
 
-import org.junit.Test;
+//import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Logger Test")
 public class LoggerRootTest {
-
+	
+	private final LoggerRoot loggerRoot = new LoggerRoot();
+	
 	@Test
+	@DisplayName("demo test😱")
+	@Disabled("test")
 	public void test1() {
 		LoggerRoot.main();
 	}
 	
+	@Test
+	void execptionTesting() {
+		Exception exception = assertThrows(ArithmeticException.class, () ->  
+			loggerRoot.divide(1,  0) );
+		
+		assertEquals("/ by zero", exception.getMessage());
+	}
 }
+	
